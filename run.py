8ed24dc774da1f15a18fc00aaf71eaf30bd4133a
will-dml <will_ou@outlook.fr>
2022-01-03 14:01:03 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -113,14 +113,29 @@ def get_last_5_entries_sales():
     a list of lists
     """
     sales = SHEET.worksheet('sales')
-    #column = sales.col_values(3)
-    #print(column)
 
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns)
+
+    return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+    
 
 
 
@@ -133,9 +148,11 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automatation")
 
-# main()
+main()
 
-get_last_5_entries_sales()
